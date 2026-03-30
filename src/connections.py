0d342071bb560f9346de6702ef0856da3014b353
ogee <ermmaculate97@gmail.com>
2026-03-30 03:51:55 +0100

    connections set

---
+++ src/connections.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL as sa_URL
+from snowflake.sqlalchemy import URL
+
+load_dotenv()
+
+# source credentials
+def get_postgres_engine():
+    url=sa_URL.create(
+    driver=("postgresql+psycopg2"),
+    username=os.getenv("db_user"),
+    password=os.getenv("db_password"),
+    host=os.getenv("db_host"),
+    port=int(os.getenv("db_port")),
+    database=os.getenv("db_database"),
+    query={"sslmode": "require"}
+    )
+    return create_engine(url)
+
+
+ # destination credentials
+def get_snowflake_engine():
+    url=URL(
+    user=os.getenv("snowflake_user"),
+    password=os.getenv("snowflake_password"),
+    account=os.getenv("snowflake_account"),
+    warehouse=os.getenv("snowflake_warehouse"),
+    database=os.getenv("snowflake_database"),
+    schema=os.getenv("snowflake_schema")
+    )
+    return create_engine(url)
