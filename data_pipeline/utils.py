3a76c6b2b3e670d131a617b7e6d441058474d1f8
Immaculate Okoro <ermmaculate97@gmail.com>
2026-04-12 08:42:51 +0100

    Add function to prefix table names with 'stg_'
    Implemented a function to add 'stg_' prefix to table names in a given dictionary and added logging for the operation.

--- data_pipeline/utils.py
+++ data_pipeline/utils.py
@@ -1,1 +1,17 @@
 
+from .logging_config import get_logger
+
+logger = get_logger("utils")
+
+def add_stg_prefix(query_dict: dict) -> dict:
+    """
+    Add stg_ prefix to table names
+    """
+    table_name = {}
+
+    for key, value in query_dict.items():
+        new_name = f"stg_{key}"
+        table_name[new_name] = value
+
+    logger.info("Added stg_ prefix to table names")
+    return table_name
