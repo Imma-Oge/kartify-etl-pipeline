3bea7b2e13f148bb1acf97ba9476ba73763fb808
Immaculate Okoro <ermmaculate97@gmail.com>
2026-04-12 08:41:42 +0100

    Add logging to create_queries function

--- data_pipeline/queries.py
+++ data_pipeline/queries.py
@@ -1,1 +1,15 @@
+from .logging_config import get_logger
 
+logger = get_logger("queries")
+
+def create_queries(tables: list) -> dict:
+    """
+    Create SQL queries for each table
+    """
+    query = {}
+
+    for t in tables:
+        query[t] = f"SELECT * FROM {t}"
+
+    logger.info(f"Created queries for {len(tables)} tables")
+    return query
